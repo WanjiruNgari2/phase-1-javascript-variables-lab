419a128ceb439be8340f22c4a8fabb07cd059c22
Damaris Ngari <wanjirungari2@gmail.com>
2024-11-28 16:13:28 +0300

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Code your solution in this file!
 //let companyName = 'Scuber';
-   const companyName = 'Scuber' ;
+const companyName = 'Scuber' ;
   
 
 let mostProfitableNeighborhood = 'Chelsea';
   mostProfitableNeighborhood == Chelsea;
   
-let companyCeo = 'Susan Smith';
-  
+
+  //let companyCeo = 'Susan Smith';
+     const companyCeo = 'Susan Smith';
+   console.log(companyCeo);
